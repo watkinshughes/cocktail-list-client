8fb252d5f0859a0c48acf4ca5fa0259c84044739
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2019-03-04 14:43:10 -0600

    Update manifest.json fix service worker setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./components/App";
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
