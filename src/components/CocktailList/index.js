03c2bb1f938b48cb79332ad766b368cda465ec7c
Ryan Watkins-Hughes <ryanwh@properinteractive.com>
2019-03-09 11:55:16 -0600

    Single filter working

--- src/components/CocktailList/index.js
+++ src/components/CocktailList/index.js
@@ -18,8 +18,7 @@ const GET_COCKTAIL_NAMES = gql`
 class CocktailList extends Component {
   state = {
     data: {
-      ingredients_contains: "",
-      name_contains: ""
+      cocktails: []
     }
   };
 
@@ -27,7 +26,7 @@ class CocktailList extends Component {
     const typedWord = event.target.value
     this.setState({
       data: {
-        ingredients_contains: typedWord,
+        // ingredients_contains: typedWord,
         name_contains: typedWord
       }
     });
@@ -36,7 +35,7 @@ class CocktailList extends Component {
   render() {
     return (
       <section className={styles.list}>
-        <form onChange={this.filterList}>
+        <form>
           <fieldset>
             <label>
               <span className="visually-hidden">
@@ -46,40 +45,40 @@ class CocktailList extends Component {
                 type="text"
                 className="Search"
                 placeholder="Filter by name or search by ingredient"
+                onChange={this.filterList}
               />
             </label>
           </fieldset>
         </form>
         <Query
-          query={GET_COCKTAIL_NAMES}
-          variables={{
-            sort: "name: ASC",
-            where: {
-              name_contains: this.state.data.name_contains,
-              ingredients_contains: this.state.data.ingredients_contains
-            }
-          }}
-        >
-        
-        {({ loading, error, data }) => {
-          if (loading) return null;
-          if (error) return `Error: ${error}`;
-          return (
-            <div>
-              {data.cocktails.map(({ name, _id, ingredients }) => {
-                return (
-                  <Cocktail
-                    key={_id}
-                    id={_id}
-                    name={name}
-                    ingredients={ingredients}
-                  />
-                );
-              })}
-            </div>
-          );
-        }}
+            query={GET_COCKTAIL_NAMES}
+            variables={{
+              sort: "name: ASC",
+              where: {
+                name_contains: this.state.data.name_contains,
+                ingredients_contains: this.state.data.ingredients_contains
+              }
+            }}
+          >
           
+          {({ loading, error, data }) => {
+            if (loading) return null;
+            if (error) return `Error: ${error}`;
+            return (
+              <div>
+                {data.cocktails.map(({ name, _id, ingredients }) => {
+                  return (
+                    <Cocktail
+                      key={_id}
+                      id={_id}
+                      name={name}
+                      ingredients={ingredients}
+                    />
+                  );
+                })}
+              </div>
+            );
+          }}
         </Query>
       </section>
     );
