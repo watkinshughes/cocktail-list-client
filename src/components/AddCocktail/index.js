63a81fe6b19bfdb7d983f70aa0546865c81416b3
Ryan Watkins-Hughes <ryanwatkinshughes@gmail.com>
2019-08-12 17:00:23 -0500

    Continue converting to Hooks

--- src/components/AddCocktail/index.js
+++ src/components/AddCocktail/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import styles from "./styles.module.css";
@@ -33,143 +33,125 @@ const CREATE_COCKTAIL = gql`
   }
 `;
 
-class AddCocktail extends Component {
-  state = {
-    name: "",
-    glass: "",
-    category: "",
-    garnish: "",
-    preparation: "",
-    ingredients: ""
+export default function AddCocktail(props) {
+  const [name, setName] = useState("");
+  const [glass, setGlass] = useState("");
+  const [category, setCategory] = useState("");
+  const [garnish, setGarnish] = useState("");
+  const [preparation, setPreparation] = useState("");
+  const [ingredients, setIngredients] = useState("");
+
+  const handleNameChange = event => {
+    setName(event.target.value);
   };
 
-  handleNameChange = event => {
-    this.setState({
-      name: event.target.value
-    });
+  const handleGlassChange = event => {
+    setGlass(event.target.value);
   };
 
-  handleGlassChange = event => {
-    this.setState({
-      glass: event.target.value
-    });
+  const handleCategoryChange = event => {
+    setCategory(event.target.value);
   };
 
-  handleCategoryChange = event => {
-    this.setState({
-      category: event.target.value
-    });
+  const handleGarnishChange = event => {
+    setGarnish(event.target.value);
   };
 
-  handleGarnishChange = event => {
-    this.setState({
-      garnish: event.target.value
-    });
+  const handlePreparationChange = event => {
+    setPreparation(event.target.value);
   };
 
-  handlePreparationChange = event => {
-    this.setState({
-      preparation: event.target.value
-    });
+  const handleIngredientsChange = event => {
+    setIngredients(event.target.value);
   };
 
-  handleIngredientsChange = event => {
-    this.setState({
-      ingredients: event.target.value
-    });
-  };
-
-  render() {
-    return (
-      <Mutation
-        mutation={CREATE_COCKTAIL}
-        variables={{
-          name: this.state.name,
-          ingredients: this.state.ingredients,
-          glass: this.state.glass,
-          garnish: this.state.garnish,
-          category: this.state.category,
-          preparation: this.state.preparation
-        }}
-        onCompleted={() => {
-          this.props.history.push("/");
-        }}
-      >
-        {(createCocktail, { loading, error }) => {
-          if (loading) return null;
-          if (error) return `Error: ${error}`;
-          return (
-            <form className={styles.form} onSubmit={createCocktail}>
-              <label>
-                <span className="visually-hidden">Name</span>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  onChange={this.handleNameChange}
-                />
-              </label>
-              <label>
-                <span className="visually-hidden">Category</span>
-                <select name="category" onChange={this.handleCategoryChange}>
-                  <option value="">Select Category</option>
-                  <option value="Before Dinner Cocktail">
-                    Before Dinner Cocktail
-                  </option>
-                  <option value="After Dinner Cocktail">
-                    After Dinner Cocktail
-                  </option>
-                  <option value="All Day Cocktail">All Day Cocktail</option>
-                  <option value="Longdrink">Longdrink</option>
-                  <option value="Sparkling Cocktail">Sparkling Cocktail</option>
-                </select>
-              </label>
-              <label>
-                <span className="visually-hidden">Glass</span>
-                <select name="glass" onChange={this.handleGlassChange}>
-                  <option value="">Select Glass</option>
-                  <option value="martini">Martini</option>
-                  <option value="old-fashioned">Old-fashioned</option>
-                  <option value="collins">Collins</option>
-                  <option value="highball">Highball</option>
-                  <option value="champagne">Champagne</option>
-                  <option value="margarita">Margarita</option>
-                </select>
-              </label>
-              <label>
-                <span className="visually-hidden">Garnish</span>
-                <input
-                  type="text"
-                  placeholder="Garnish"
-                  name="granish"
-                  onChange={this.handleGarnishChange}
-                />
-              </label>
-              <label>
-                <span className="visually-hidden">Preparation</span>
-                <textarea
-                  name="preparation"
-                  placeholder="Preparation"
-                  onChange={this.handlePreparationChange}
-                />
-              </label>
-              <label>
-                <span className="visually-hidden">Ingredients</span>
-                <textarea
-                  name="ingredients"
-                  placeholder="Ingredients"
-                  onChange={this.handleIngredientsChange}
-                />
-              </label>
-              <button className={styles.button} type="submit">
-                Submit
-              </button>
-            </form>
-          );
-        }}
-      </Mutation>
-    );
-  }
+  return (
+    <Mutation
+      mutation={CREATE_COCKTAIL}
+      variables={{
+        name: name,
+        ingredients: ingredients,
+        glass: glass,
+        garnish: garnish,
+        category: category,
+        preparation: preparation
+      }}
+      onCompleted={() => {
+        props.history.push("/");
+      }}
+    >
+      {(createCocktail, { loading, error }) => {
+        if (loading) return null;
+        if (error) return `Error: ${error}`;
+        return (
+          <form className={styles.form} onSubmit={createCocktail}>
+            <label>
+              <span className="visually-hidden">Name</span>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={handleNameChange}
+              />
+            </label>
+            <label>
+              <span className="visually-hidden">Category</span>
+              <select name="category" onChange={handleCategoryChange}>
+                <option value="">Select Category</option>
+                <option value="Before Dinner Cocktail">
+                  Before Dinner Cocktail
+                </option>
+                <option value="After Dinner Cocktail">
+                  After Dinner Cocktail
+                </option>
+                <option value="All Day Cocktail">All Day Cocktail</option>
+                <option value="Longdrink">Longdrink</option>
+                <option value="Sparkling Cocktail">Sparkling Cocktail</option>
+              </select>
+            </label>
+            <label>
+              <span className="visually-hidden">Glass</span>
+              <select name="glass" onChange={handleGlassChange}>
+                <option value="">Select Glass</option>
+                <option value="martini">Martini</option>
+                <option value="old-fashioned">Old-fashioned</option>
+                <option value="collins">Collins</option>
+                <option value="highball">Highball</option>
+                <option value="champagne">Champagne</option>
+                <option value="margarita">Margarita</option>
+              </select>
+            </label>
+            <label>
+              <span className="visually-hidden">Garnish</span>
+              <input
+                type="text"
+                placeholder="Garnish"
+                name="granish"
+                onChange={handleGarnishChange}
+              />
+            </label>
+            <label>
+              <span className="visually-hidden">Preparation</span>
+              <textarea
+                name="preparation"
+                placeholder="Preparation"
+                onChange={handlePreparationChange}
+              />
+            </label>
+            <label>
+              <span className="visually-hidden">Ingredients</span>
+              <textarea
+                name="ingredients"
+                placeholder="Ingredients"
+                onChange={handleIngredientsChange}
+              />
+            </label>
+            <button className={styles.button} type="submit">
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </Mutation>
+  );
 }
-
-export default AddCocktail;
